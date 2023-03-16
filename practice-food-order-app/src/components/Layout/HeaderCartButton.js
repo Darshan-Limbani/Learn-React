10f250d50070268e27dff3a47a0e439ea84eec29
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-16 13:42:39 +0530

    Added Animation to the Cart Button. Completed The Project

--- practice-food-order-app/src/components/Layout/HeaderCartButton.js
+++ practice-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,23 +1,40 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css'
 import CartContext from "../../store/cart-context";
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 
 const HeaderCartButton = props => {
-    const cartCtx = useContext(CartContext);
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+    const {items} = useContext(CartContext);
 
-    console.log("=========", cartCtx.items)
+    // console.log("=========", cartCtx.items)
 
-    const numberOfCartItems = cartCtx.items.reduce((curNum, item) => {
+    const numberOfCartItems = items.reduce((curNum, item) => {
         return curNum + item.amount
     }, 0)
 
     // console.log("HeaderCartButton : ", props.onClick)
     // console.log("HeaderCartButton : ", numberOfCartItems)
     // console.log("HeaderCartButton : ", cartCtx.items)
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true)
+
+        const timer = setTimeout(() => setBtnIsHighlighted(false), 300)
+
+        return () => {
+            clearTimeout(timer)
+        }
+
+    }, [items]);
+
 
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon/>
             </span>
