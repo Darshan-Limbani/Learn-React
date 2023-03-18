e2092e755ee225a46e58c1ec83db966da21f4524
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-18 10:01:39 +0530

    Added basic validation

--- 16-working-with-forms/src/components/SimpleInput.js
+++ 16-working-with-forms/src/components/SimpleInput.js
@@ -15,22 +15,24 @@ const SimpleInput = (props) => {
 
     const formSubmissionHandler = event => {
         event.preventDefault()
-        setEnteredName('')
+
+        if (enteredName.trim() === '') {
+            return;
+        }
+
         console.log(enteredName)
         console.log(nameInputRef.current.value)
     }
 
-    return (
-        <form onSubmit={formSubmissionHandler}>
-            <div className='form-control'>
-                <label htmlFor='name'>Your Name</label>
-                <input ref={nameInputRef} type='text' id='name' value={enteredName} onChange={nameInputChangeHandler}/>
-            </div>
-            <div className="form-actions">
-                <button>Submit</button>
-            </div>
-        </form>
-    );
+    return (<form onSubmit={formSubmissionHandler}>
+        <div className='form-control'>
+            <label htmlFor='name'>Your Name</label>
+            <input ref={nameInputRef} type='text' id='name' value={enteredName} onChange={nameInputChangeHandler}/>
+        </div>
+        <div className="form-actions">
+            <button>Submit</button>
+        </div>
+    </form>);
 };
 
 export default SimpleInput;
