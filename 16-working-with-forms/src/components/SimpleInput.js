b5a5c86061b808f490780d547d1883ac3b678ab8
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-18 10:00:11 +0530

    Added Basic From Component

--- 16-working-with-forms/src/components/SimpleInput.js
+++ 16-working-with-forms/src/components/SimpleInput.js
@@ -1,15 +1,36 @@
+import {useRef, useState} from "react";
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+
+    const nameInputRef = useRef()
+    const [enteredName, setEnteredName] = useState('');
+
+
+    const nameInputChangeHandler = event => {
+        console.log(event.target.value)
+        setEnteredName(event.target.value)
+        // console.log(nameInputRef.current.value)
+    }
+
+
+    const formSubmissionHandler = event => {
+        event.preventDefault()
+        setEnteredName('')
+        console.log(enteredName)
+        console.log(nameInputRef.current.value)
+    }
+
+    return (
+        <form onSubmit={formSubmissionHandler}>
+            <div className='form-control'>
+                <label htmlFor='name'>Your Name</label>
+                <input ref={nameInputRef} type='text' id='name' value={enteredName} onChange={nameInputChangeHandler}/>
+            </div>
+            <div className="form-actions">
+                <button>Submit</button>
+            </div>
+        </form>
+    );
 };
 
 export default SimpleInput;
