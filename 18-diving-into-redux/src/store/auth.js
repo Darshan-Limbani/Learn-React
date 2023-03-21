9bd8076ce30e48c114741635643efffa9e01b232
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-21 15:18:40 +0530

    Splitting out the Code.

---
+++ 18-diving-into-redux/src/store/auth.js
@@ -0,0 +1,23 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initAuthState = {
+    isAuthenticated: false
+}
+
+
+const authSlice = createSlice({
+    name: 'authentication',
+    initialState: initAuthState,
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true
+        },
+        logout(state) {
+            state.isAuthenticated = false
+        }
+    }
+})
+
+export const authActions = authSlice.actions
+export const authReducer =  authSlice.reducer
+
