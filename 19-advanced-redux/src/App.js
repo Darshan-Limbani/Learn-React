def99673b766b9b7aeba6259e49914d8985b4ddc
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-21 19:16:10 +0530

    Handling Http states Feedback with Redux

--- 19-advanced-redux/src/App.js
+++ 19-advanced-redux/src/App.js
@@ -1,14 +1,73 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import {useDispatch, useSelector} from "react-redux";
+import {Fragment, useEffect} from "react";
+import {uiActions} from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
-  return (
-    <Layout>
-      <Cart />
-      <Products />
-    </Layout>
-  );
+
+    const cart = useSelector((state) => state.cart)
+    const showCart = useSelector(state => state.ui.cartIsVisible)
+    const notification = useSelector(state => state.ui.notification)
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const sendCartData = async () => {
+
+
+            const response = await fetch('https://react-http-7c896-default-rtdb.firebaseio.com/cart.json', {
+                method: 'PUT',
+                body: JSON.stringify(cart)
+            })
+
+            if (!response.ok) {
+                throw new Error('Sending cart data failed.')
+            }
+
+            dispatch(uiActions.showNotification({
+                status: 'success',
+                title: 'Success!',
+                message: 'Sent cart data successfully!',
+            }))
+
+            const data = await response.json();
+        }
+
+        if (isInitial) {
+            isInitial = false;
+            return
+        }
+
+        sendCartData().catch(err => {
+            dispatch(uiActions.showNotification({
+                status: 'error',
+                title: 'Error!',
+                message: 'Sending cart failed!',
+            }))
+        })
+    }, [cart, dispatch]);
+
+
+    return (
+
+        <Fragment>
+            {notification &&
+                <Notification
+                    status={notification.status}
+                    title={notification.title}
+                    message={notification.message}
+                />}
+            <Layout>
+                {showCart && <Cart/>}
+                <Products/>
+            </Layout>
+        </Fragment>
+    );
 }
 
 export default App;
--- 19-advanced-redux/src/components/Cart/CartButton.js
+++ 19-advanced-redux/src/components/Cart/CartButton.js
@@ -1,12 +1,22 @@
 import classes from './CartButton.module.css';
+import {useDispatch, useSelector} from "react-redux";
+import {uiActions} from "../../store/ui-slice";
 
 const CartButton = (props) => {
-  return (
-    <button className={classes.button}>
-      <span>My Cart</span>
-      <span className={classes.badge}>1</span>
-    </button>
-  );
+
+    const dispatch = useDispatch();
+
+    const totalQuantity = useSelector(state => state.cart.totalQuantity)
+    const toggleCartHandler = () => {
+        dispatch(uiActions.toggle())
+    }
+
+    return (
+        <button className={classes.button} onClick={toggleCartHandler}>
+            <span>My Cart</span>
+            <span className={classes.badge}>{totalQuantity}</span>
+        </button>
+    );
 };
 
 export default CartButton;
