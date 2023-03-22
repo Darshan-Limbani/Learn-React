886dbc49e0420e6496dca89915b6ac99b4ae26e0
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-22 19:15:33 +0530

    Added Prompt component to prevent unwanted routes

--- 20-quotes-project/src/components/quotes/QuoteForm.js
+++ 20-quotes-project/src/components/quotes/QuoteForm.js
@@ -1,10 +1,14 @@
-import {useRef} from 'react';
+import {Fragment, useRef, useState} from 'react';
 
 import classes from './QuoteForm.module.css';
 import Card from "../UI/Card";
 import LoadingSpinner from "../UI/LoadingSpinner";
+import {Prompt} from "react-router-dom";
 
 const QuoteForm = (props) => {
+
+    const [isChanged, setIsChanged] = useState(false);
+
     const authorInputRef = useRef();
     const textInputRef = useRef();
 
@@ -14,45 +18,52 @@ const QuoteForm = (props) => {
         const enteredAuthor = authorInputRef.current.value;
         const enteredText = textInputRef.current.value;
 
-
-        if(enteredAuthor.trim().length === 0)
-        {
+        if (enteredAuthor?.trim().length === 0) {
             alert('Author Name is Required!!')
             return
         }
-        if(enteredText.trim().length === 0)
-        {
+        if (enteredText?.trim().length === 0) {
             alert('Quotes Text is Required!!')
             return;
         }
 
-
-
-
+        // setIsChanged(true)
         // optional: Could validate here
-
         props.onAddQuote({author: enteredAuthor, text: enteredText});
     }
 
-    return (<Card>
-            <form className={classes.form} onSubmit={submitFormHandler}>
-                {props.isLoading && (<div className={classes.loading}>
-                        <LoadingSpinner/>
-                    </div>)}
+    const finishChangedHandler = () => {
+        setIsChanged(false)
+    }
 
-                <div className={classes.control}>
-                    <label htmlFor='quote_author'>Author</label>
-                    <input type='text' id='quote_author' ref={authorInputRef}/>
-                </div>
-                <div className={classes.control}>
-                    <label htmlFor='text'>Text</label>
-                    <textarea id='text' rows='5' ref={textInputRef}></textarea>
-                </div>
-                <div className={classes.actions}>
-                    <button className='btn'>Add Quote</button>
-                </div>
-            </form>
-        </Card>);
+    const formFocusedHandler = () => {
+        setIsChanged(true)
+    }
+
+    return (<Fragment>
+            <Prompt when={isChanged}
+                    message={(location) => 'Are you sure you want to exit? All your data will be lost'}/>
+                <Card>
+                    <form className={classes.form} onSubmit={submitFormHandler} onFocus={formFocusedHandler}>
+                        {props.isLoading && (<div className={classes.loading}>
+                            <LoadingSpinner/>
+                        </div>)}
+                        <div className={classes.control}>
+                            <label htmlFor='quote_author'>Author</label>
+                            <input type='text' id='quote_author' ref={authorInputRef}/>
+                        </div>
+                        <div className={classes.control}>
+                            <label htmlFor='text'>Text</label>
+                            <textarea id='text' rows='5' ref={textInputRef}></textarea>
+                        </div>
+                        <div className={classes.actions}>
+                            <button className='btn' onClick={finishChangedHandler}>Add Quote</button>
+                        </div>
+                    </form>
+                </Card>
+
+        </Fragment>
+    );
 };
 
 export default QuoteForm;
