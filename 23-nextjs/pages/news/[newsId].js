cf95cfe084e2ff1feca205ae58ac07ecc8fc0ad7
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-24 17:35:59 +0530

    Extracting Dynamic Parameter

--- 23-nextjs/pages/news/[newsId].js
+++ 23-nextjs/pages/news/[newsId].js
@@ -1,4 +1,11 @@
-function News(){
+import {useRouter} from "next/router";
+
+function News() {
+
+    const router = useRouter()
+    const newsId = router.query.newsId;
+    console.log(newsId)
     return <p>Detail News</p>
 }
+
 export default News
