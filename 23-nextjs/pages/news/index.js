ae6d429968bde36c660284711f3caa9464e10898
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-24 17:52:31 +0530

    Linking between Pages

--- 23-nextjs/pages/news/index.js
+++ 23-nextjs/pages/news/index.js
@@ -1,6 +1,21 @@
+import {Fragment} from "react";
+import Link from 'next/link'
+
 function NewsPage() {
     return (
-        <p>This is News Page</p>
+        <Fragment>
+            <p>This is News Page</p>
+            <ul>
+                <li>
+                    <Link href={'/news/nextjs-is-great-framework'}>
+                        nextjs is great framework
+                    </Link>
+                </li>
+                <li>
+                    Something Else
+                </li>
+            </ul>
+        </Fragment>
     )
 }
 
