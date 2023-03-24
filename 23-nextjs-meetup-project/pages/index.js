a3661751d4d239972991e45675e9007b63c749cf
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-24 19:45:14 +0530

    data Fetching from Static Pages

--- 23-nextjs-meetup-project/pages/index.js
+++ 23-nextjs-meetup-project/pages/index.js
@@ -1,29 +1,32 @@
 import MeetupList from "@/components/meetups/MeetupList";
 
-const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_München.jpg/1280px-Stadtbild_München.jpg',
-        title: 'A First Meetup',
-        address: 'Science Center,Surat',
-        description: "This is first meetup!"
+const DUMMY_MEETUPS = [{
+    id: 'm1',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_München.jpg/1280px-Stadtbild_München.jpg',
+    title: 'A First Meetup',
+    address: 'Science Center,Surat',
+    description: "This is first meetup!"
+}, {
+    id: 'm2',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_München.jpg/1280px-Stadtbild_München.jpg',
+    title: 'A Second Meetup',
+    address: 'Marine Drive,Mumbai',
+    description: "This is Second meetup!"
+}]
 
-    },
-    {
-        id: 'm2',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_München.jpg/1280px-Stadtbild_München.jpg',
-        title: 'A Second Meetup',
-        address: 'Marine Drive,Mumbai',
-        description: "This is Second meetup!"
 
+function HomePage(props) {
+
+    return <MeetupList meetups={props.meetups}/>
+
+}
+
+export async function getStaticProps() {
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS
+        }
     }
-]
-
-
-function HomePage() {
-
-    return <MeetupList meetups={DUMMY_MEETUPS}/>
-
 }
 
 export default HomePage
