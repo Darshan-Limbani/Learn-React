aada9f20660c480c791b309923a740508d42177d
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-24 19:59:30 +0530

    Exploring Server Side Rendering (SSR) _getServerSIdeProps_

--- 23-nextjs-meetup-project/pages/index.js
+++ 23-nextjs-meetup-project/pages/index.js
@@ -21,13 +21,27 @@ function HomePage(props) {
 
 }
 
-export async function getStaticProps() {
-    return {
-        props: {
-            meetups: DUMMY_MEETUPS
-        },
-        revalidate:1
+export async function getServerSideProps(context){
+
+    const req = context.req;
+    const res = context.res;
+
+    return{
+        props:{
+            meetups : DUMMY_MEETUPS
+        }
     }
 }
+
+
+// export async function getStaticProps(context) {
+//
+//     return {
+//         props: {
+//             meetups: DUMMY_MEETUPS
+//         },
+//         revalidate:1
+//     }
+// }
 
 export default HomePage
