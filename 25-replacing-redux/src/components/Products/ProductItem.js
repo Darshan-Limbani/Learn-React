65d223f5a7abeea3fd1ff4a5fe9a73590e2a2833
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-27 17:22:36 +0530

    Getting Started with Custom Hook as a Store

--- 25-replacing-redux/src/components/Products/ProductItem.js
+++ 25-replacing-redux/src/components/Products/ProductItem.js
@@ -14,17 +14,17 @@ const ProductItem = props => {
     };
 
     return (<Card style={{marginBottom: '1rem'}}>
-            <div className="product-item">
-                <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
-                <p>{props.description}</p>
-                <button
-                    className={!props.isFav ? 'button-outline' : ''}
-                    onClick={toggleFavHandler}
-                >
-                    {props.isFav ? 'Un-Favorite' : 'Favorite'}
-                </button>
-            </div>
-        </Card>);
+        <div className="product-item">
+            <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
+            <p>{props.description}</p>
+            <button
+                className={!props.isFav ? 'button-outline' : ''}
+                onClick={toggleFavHandler}
+            >
+                {props.isFav ? 'Un-Favorite' : 'Favorite'}
+            </button>
+        </div>
+    </Card>);
 };
 
 export default ProductItem;
