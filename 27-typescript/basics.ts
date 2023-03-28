9d9931db9e4df9dc1264404dc06a2d0b12437ab2
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-28 13:47:24 +0530

    Exploring Base Type

---
+++ 27-typescript/basics.ts
@@ -0,0 +1,18 @@
+// Primitive : number, string, boolean
+// More complex type : arrays, objects
+// Function types, parameter
+
+
+//Primitives
+
+let age: number;
+
+age = 21;
+
+let userName: string;
+
+userName = "Darshan";
+
+let isInstructor: boolean;
+
+isInstructor = false
