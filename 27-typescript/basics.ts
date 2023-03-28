3f3f181870c402e382f60fd82456d4150467cb40
Darshan Limbani <darshanlimbani94@gmail.com>
2023-03-28 13:56:34 +0530

    Working with Array, Object Types

--- 27-typescript/basics.ts
+++ 27-typescript/basics.ts
@@ -16,3 +16,30 @@ userName = "Darshan";
 let isInstructor: boolean;
 
 isInstructor = false
+
+// More complex types
+
+let person: {
+    name: string,
+    age: number
+    friends: string[]
+}
+
+person = {
+    name: 'darshan',
+    age: 21,
+    friends: ['udit', 'deep']
+}
+
+let hobbies: string[];
+
+hobbies = ['cricket', 'carrom']
+
+let people:{
+    name : string,
+    age:number
+}[]
+
+
+
+
